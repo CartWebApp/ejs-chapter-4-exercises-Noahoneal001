42fb0a63601bf91b14c44cd763d586dc6088335c
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-01-31 14:41:49 -0800

    LIST IS DONE

--- list.js
+++ list.js
@@ -1,17 +1,28 @@
 function arrayToList(arr) {
-  // Add code.
+let output = null;
+let length = arr.length;
+for (let counter = length - 1; counter >= 0; counter--) {
+  output = {value: arr[counter], rest: output}
+}
+return output;
 }
 
 function listToArray(list, arr) {
   // Add code.
+  let keyring = Object.keys(list);
+  arr = []; 
+  for (let counter = list; counter; counter = counter.rest) {
+    arr.push(counter.value)
+  }
+  return arr
 }
 
 // tests
-console.log(arrayToList([10, 20]));
+console.log(arrayToList([1, 2, 3]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
-console.log(prepend(10, prepend(20, null)));
+// console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(nth(arrayToList([10, 20, 30]), 1));
+// console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
