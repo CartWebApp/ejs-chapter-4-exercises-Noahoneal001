9c5ed85963a009c4665ec43a54442ff872f02221
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-02-01 14:50:45 -0800

    Deep equals done

--- deep-equal.js
+++ deep-equal.js
@@ -1,25 +1,21 @@
 function deepEqual(value, reference) {
   // Add code.
-  let output = false;
   let lastHopes1 = ``;
   let lastHopes2 = ``;
-  let holder1 = Object.values(value)
-  let holder2 = Object.values(reference)
+  let holder1 = Object.values(value);
+  let holder2 = Object.values(reference);
   let keyring1 = Object.keys(value);
   let keyring2 = Object.keys(reference);
-  console.log(`Holder 1: `, holder1)
-  console.log(`Holder 2: `, holder2)
   if (value === reference) {
-    return true
-  }
-    for (const counter of keyring1) {
-      if (typeof reference[counter] == `object` && reference[counter] != null) {
-        lastHopes1 = Object.values(reference[counter])
-        lastHopes2 = Object.values(value[counter])
-        console.log(lastHopes1.toString() === lastHopes2.toString())
-        return true
-      }
-    }
+    return true;
+  };
+  for (const counter of keyring1) {
+    if (typeof reference[counter] == `object` && reference[counter] != null) {
+      lastHopes1 = Object.values(reference[counter]);
+      lastHopes2 = Object.values(value[counter]);
+      return lastHopes1.toString() === lastHopes2.toString();
+    };
+  };
   return false;
 };
 // tests
